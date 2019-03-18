7e07cc3679257834d889968c5649a3be1e110261
tatiana.mojica <tatiana.mojica@bustle.com>
2019-03-18 23:15:50 +0000

    Automatically backed up by Learn

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,7 +1,7 @@
 class StudentController < ApplicationController 
   
   def index 
-    @student = Student.all 
+    @students = Student.all 
     #pulling in a list of all students 
   end 
   # controller action for handling index action
